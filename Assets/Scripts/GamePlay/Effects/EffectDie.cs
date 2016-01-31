3833a17720d16c986ec8325a118f5708541e66a7
AlKoulounil <lucasbaire@yahoo.fr>
2016-01-31 22:33:10 +0100

    Issues #21, #22, #27, #28, #36

---
+++ Assets/Scripts/GamePlay/Effects/EffectDie.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Triggers;
+using Containers;
+
+namespace Effects
+{
+	public class EffectDie : MonoBehaviour, IEffect
+	{
+
+		public void Apply (Dictionary<STORAGE_KEYS, Storage> targets)
+		{
+			// TODO
+		}
+
+	}
+}
---
+++ Assets/Scripts/GamePlay/Effects/EffectLog.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Triggers;
+using Containers;
+
+namespace Effects
+{
+	public class EffectLog : MonoBehaviour, IEffect
+	{
+		public string Message = "Type message to Log Here";
+
+		public void Apply(Dictionary<STORAGE_KEYS, Storage> targets) {
+			Debug.Log (Message);
+		}
+	}
+}
+
