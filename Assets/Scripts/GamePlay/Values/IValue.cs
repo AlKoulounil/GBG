06a2e21842b3ff23cebb8b75dc86fd813c0b733d
AlKoulounil <lucasbaire@yahoo.fr>
2016-01-31 20:26:53 +0100

    Issue #44 interface IValue

---
+++ Assets/Scripts/GamePlay/Values/IValue.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IValue {
+
+	float GetValue ();
+	string GetName ();
+}
--- Assets/Scripts/GamePlay/Values/Stock.cs
+++ Assets/Scripts/GamePlay/Values/Stock.cs
@@ -6,10 +6,10 @@ using Resources;
 namespace Values
 {
 
-	public class Stock : MonoBehaviour
+	public class Stock : MonoBehaviour, IValue
 	{
 
-		public Resource Type;
+		public Resource Res;
 		public float Value;
 
 
@@ -19,11 +19,19 @@ namespace Values
 		public float Maximum = 0;
 
 		public void SetValue(float val) {
-			if (Type.IsInteger) {
+			if (Res.IsInteger) {
 				this.Value = (int)Mathf.Floor (val);
 			} else {
 				this.Value = val;
 			}
+		}
+
+		public float GetValue() {
+			return this.Value;
+		}
+
+		public string GetName() {
+			return Res.ResourceName;
 		}
 	}
 }
