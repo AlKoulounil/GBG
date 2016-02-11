c67b1089725bb975b9489622abb9d7806495ac72
AlKoulounil <lucasbaire@yahoo.fr>
2016-02-11 20:32:57 +0100

    issue #35 - rename Formula to AFormula and create its sister classes

---
+++ Assets/Scripts/GamePlay/Calculator/BoolFormula.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace PUT_NAMESPACE_HERE {
+
+	public class BoolFormula : MonoBehaviour {
+
+	}
+}
--- Assets/Scripts/GamePlay/Calculator/FloatFormula.cs
+++ Assets/Scripts/GamePlay/Calculator/FloatFormula.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace PUT_NAMESPACE_HERE {
 
-	public class #SCRIPTNAME# : MonoBehaviour {
+	public class FloatFormula : MonoBehaviour {
 
 	}
 }
